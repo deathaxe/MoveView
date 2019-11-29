4234a618b3a74e22e0ee2eecbdede0e21ba8008f
DeathAxe <deathaxe@googlemail.com>
2019-11-29 20:45:37 +0100

    Rewrite plugin and rename to MoveView

--- README.md
+++ README.md
@@ -1,32 +1,39 @@
-Move Tab
-========
+# Move View
 
-Plugin for Sublime Text to move tabs around.
+Plugin for Sublime Text to move views within a group.
 
-Usage
------
+![Animation](https://user-images.githubusercontent.com/16542113/69913166-7579e880-1434-11ea-92c8-80c04a0d632e.gif)
 
-The following commands are accessible via the command palette:
+It is heavily inspired by [MoveTab](https://github.com/SublimeText/MoveTab).
 
-- Move Tab: To the left
-- Move Tab: To the right
-- Move Tab: To first position
-- Move Tab: To last position
+It is written from scratch in order to...
 
-The default shortcuts are:
+1. introduce a wording (Move View) which better fits into Sublime Text's ecosystem as there are related functions like `Menu > View > Move File To Group` for instance.
+2. introduce new commands `move_to_neighboring_index` and `move_to_index`, which sound and behave like the built-in `move_to_neighbouring_group` and `move_to_group` commands.
+3. enable `move_to_index` to list all open files of the current group as a possible destination to move the active view to.
 
-- Linux/Windows: CTRL + Shift + (Page up / Page down)
-- MacOS X: Command + Alt + Shift + (Left / Right)
 
-*Shortcuts to move to first or last position are disabled by default.*
+## Usage
 
-Requirements
-------------
+A view can be moved via Command Palette with:
 
-Sublime Text 2 or 3.
+- Move View: To Positon
+- Move View: To The Left
+- Move View: To The Right
+- Move View: To First Position
+- Move View: To Last Position
 
-Installation
-------------
+A view can be moved via keyboard with:
+
+- Linux/Windows: <kbd>CTRL + Shift + Page up</kbd> and <kbd>CTRL + Shift + Page down</kbd>
+- MacOS: <kbd>Command + Alt + Shift + Left</kbd> and <kbd>Command + Alt + Shift + Right</kbd>
+
+
+## Requirements
+
+Sublime Text 3.
+
+## Installation
 
 Use the Package Control.
 
@@ -36,7 +43,6 @@ Alternatively, simply clone this repository into the Packages directory.
 
 	Preferences > Browse Packages...
 
-License
--------
+## License
 
 Licensed under the [MIT License](http://www.opensource.org/licenses/mit-license.php)
---
+++ move_view.py
@@ -0,0 +1,91 @@
+from os.path import basename
+
+import sublime_plugin
+
+__all__ = [
+    'MoveToNeighboringIndexCommand',
+    'MoveToIndexCommand'
+]
+
+
+def active_view_group_index(window):
+    view = window.active_view()
+    group, index = window.get_view_index(view)
+    return view, group, index
+
+
+class MoveToNeighboringIndexCommand(sublime_plugin.WindowCommand):
+
+    def is_enabled(self, forward=True):
+        return len(self.window.views_in_group(self.window.active_group())) > 1
+
+    def run(self, forward=True):
+        view, view_group, view_index = active_view_group_index(self.window)
+        num_views = len(self.window.views_in_group(view_group))
+        if num_views > 1:
+            if forward:
+                index = (view_index + 1) % num_views
+            else:
+                index = (view_index - 1) % num_views
+            if index != view_index:
+                self.window.set_view_index(view, view_group, index)
+                self.window.focus_view(view)
+
+
+class MoveToIndexCommand(sublime_plugin.WindowCommand):
+
+    def is_enabled(self, index=None):
+        if index is None:
+            return True
+        _, view_group, view_index = active_view_group_index(self.window)
+        if view_index in (-1, index):
+            return False
+        num_views = len(self.window.views_in_group(view_group))
+        return num_views > 1 and index < num_views
+
+    def input(self, args):
+        if args.get('index') is None:
+            return IndexInputHandler(self.window)
+        return None
+
+    def input_description(self):
+        return "Move after"
+
+    def run(self, index):
+        view, view_group, view_index = active_view_group_index(self.window)
+        num_views = len(self.window.views_in_group(view_group))
+        if num_views > 1:
+            if index < 0:
+                index = num_views + index
+            index = max(0, min(index, num_views - 1))
+            if index != view_index:
+                self.window.set_view_index(view, view_group, index)
+                self.window.focus_view(view)
+
+
+class IndexInputHandler(sublime_plugin.ListInputHandler):
+
+    def __init__(self, window):
+        super().__init__()
+        self.window = window
+
+    def list_items(self):
+        view, view_group, view_index = active_view_group_index(self.window)
+        views = self.window.views_in_group(view_group)
+        views.pop(view_index)
+
+        if view_index > 0:
+            items = [("First Position\tto position 1", 0)]
+        else:
+            items = []
+
+        for i, v in enumerate(views, start=1):
+            if i == view_index:
+                continue
+            try:
+                title = basename(v.file_name())
+            except TypeError:
+                title = v.name() or 'untitled'
+            items.append(("{}\tto position {}".format(title, i + 1), i))
+
+        return items
